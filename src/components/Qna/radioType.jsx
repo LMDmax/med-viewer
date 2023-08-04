67ba7786c40fb696aee70cae86de2be272cea60b
Subhojit Dutta <subhojit.dutta@softsensor.ai>
2023-08-05 03:30:54 +0530

    final check on radio type

--- src/components/Qna/radioType.jsx
+++ src/components/Qna/radioType.jsx
@@ -1,35 +1,45 @@
 import React from "react";
 import _ from "lodash";
+import { Box, Text, Flex } from "@chakra-ui/react";
 
 function RadioType({ question, response, handleChange, slideQna }) {
   return (
-    <div>
+    <Box  w="100%">
       {question?.Question?.choices?.map((choice, index) => (
         <label key={index + 1}>
-          <input
-            type="radio"
-            name={question?.Question?.id}
-            value={choice}
-            checked={
-              !_.isEmpty(response)
-                ? response[question?.Question?.id]?.choiceId === choice
-                : question?.Question?.correctAnswer &&
-                  question?.Question?.correctAnswer[0] === choice
-            }
-            onChange={(e) => {
-              handleChange({
-                questionId: question?.Question?.id,
-                choice: e.target.value,
-              });
-            }}
-            disabled={
-              !_.isEmpty(response) || question?.Question?.correctAnswer
-            }
-          />
-          <span>{choice}</span>
+          <Flex alignItems="center">
+            <input
+              type="radio"
+              name={question?.Question?.id}
+              value={choice}
+              checked={
+                !_.isEmpty(response)
+                  ? response[question?.Question?.id]?.choiceId === choice
+                  : question?.Question?.correctAnswer &&
+                    question?.Question?.correctAnswer[0] === choice
+              }
+              onChange={(e) => {
+                handleChange({
+                  questionId: question?.Question?.id,
+                  choice: e.target.value,
+                });
+              }}
+              disabled={!_.isEmpty(response) || question?.Question?.correctAnswer}
+            />
+            <Text
+              ml="10px"
+              wordBreak="break-word"
+              whiteSpace="pre-wrap"
+              maxWidth="100%"
+              overflowWrap="break-word"
+              cursor="pointer"
+            >
+              {choice}
+            </Text>
+          </Flex>
         </label>
       ))}
-    </div>
+    </Box>
   );
 }
 
